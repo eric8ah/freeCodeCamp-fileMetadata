0e4f7249b077db4347e6bdce7dc9d5e9f834450e
Eric Ochoa <eric.ochoa.usc@gmail.com>
2016-09-08 07:32:41 +0000

    Final version 1.0

--- server.js
+++ server.js
@@ -1,43 +1,30 @@
-var $ = require('jquery');
 var express = require('express');
 var app = express();
 var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+var upload = multer({ storage: storage}).single('fileUpload');
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.post('/upload', function(req, res) {
-    // this creates the incoming form object
-    var form = new formidable.IncomingForm();
-    
-    //this stores the uploads in the /uploads directory
-    form.uploadDir = path.join(__dirname, '/uploads');
-    
-    //this will rename a successfully loaded file into its original name
-    form.on('file', function(field, file) {
-        fs.rename(file.path, path.join(form.uploadDir, file.name));
+app.post('/api/upload', function(req, res) {
+    upload(req, res, function(err) {
+        if (err) {
+            return res.end('Error uploading file.');
+        }
+        var response = {size: req.file.size};
+        res.json(response);
     });
-    
-    //log errors
-    form.on('error', function(err) {
-        console.log('An error has occured: \n' + err);
-    });
-    
-    //send response to client once all files have been uploaded
-    form.on('end', function() {
-        res.end('success');
-    });
-    
-    //parses incoming request containing the form data
-    form.parse(req);
 });
-
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Server has been started');
